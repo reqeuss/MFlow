2b1d87ac4ad08af6e5986035936e35aff89a9cde
Reqeuss <143525922+reqeuss@users.noreply.github.com>
2026-09-18 14:28:22 +0000

    refactor: use native packet type in pipeline

--- core/include/mflow/pipeline.hpp
+++ core/include/mflow/pipeline.hpp
@@ -1,19 +1,12 @@
 #pragma once
 
-#include <cstddef>
-#include <cstdint>
+#include "mflow/packet.hpp"
+
 #include <functional>
-#include <memory>
 #include <string>
 #include <vector>
 
 namespace mflow {
-
-struct Packet {
-    std::shared_ptr<std::vector<std::byte>> buffer;
-    std::size_t size{0};
-    std::uint64_t sequence{0};
-};
 
 using Stage = std::function<void(Packet&)>;
 
